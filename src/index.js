146e6a1a491103f0377f9a5ee7872b2dc0225154
Tijuana (T) <tweatherspoon00@gmail.com>
2024-03-20 19:09:29 -0500

    added weather icon

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ function refreshWeather(response) {
   let speedElement = document.querySelector("#speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
   cityElement.innerHTML = response.data.city;
 
@@ -15,6 +16,7 @@ function refreshWeather(response) {
   humidityElement.innerHTMl = `${response.data.temperature.humidity}%`;
   speedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 }
 
 function formatDate(date) {
